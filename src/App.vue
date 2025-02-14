f9f08ef1c2eceff8312fd617c0261d6702858007
Cett <florian_steinmann@hotmail.de>
2025-02-14 23:46:02 +0100

    alte Typescript Dateien gelöscht

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { Authenticator } from "@aws-amplify/ui-vue";
 import "@aws-amplify/ui-vue/styles.css";
 import Todos from './components/Todos.vue';
 import Upload from './components/Upload.vue';
-import S3FileManager from './components/S3FileManager.vue';
+import FileManager from './components/FileManager.vue';
 
 
 </script>
@@ -13,7 +13,7 @@ import S3FileManager from './components/S3FileManager.vue';
   <main>
     <authenticator>
       <template v-slot="{ signOut }">
-        <S3FileManager />
+        <FileManager />
         <!-- //<Todos />
         <Upload />-->
         <div class="right-column">
